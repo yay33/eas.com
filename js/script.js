aa7dfdf210d59eb729c1fe142b3e9bb62d185a5d
qwerty_xyecoc <max6kotlov@gmail.com>
2022-12-14 21:39:11 +0500

    clock

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-console.log('123')
-
-//var modal = "#mymodal"
-//modal.on('shown.bs.modal', function () {
- // $('#myInput').trigger('focus')
-//})
-
-
-var myModal = document.getElementById('modal_cool')
-var myInput = document.getElementById('myBtn')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
-
+$(function(){
+  //button select all or cancel
+  $("#select-all").click(function () {
+      var all = $("input.select-all")[0];
+      all.checked = !all.checked
+      var checked = all.checked;
+      $("input.select-item").each(function (index,item) {
+          item.checked = checked;
+      });
+  });
+  //column checkbox select all or cancel
+  $("input.select-all").click(function () {
+      var checked = this.checked;
+      $("input.select-item").each(function (index,item) {
+          item.checked = checked;
+      });
+  });
+  //check selected items
+  $("input.select-item").click(function () {
+      var checked = this.checked;
+      var all = $("input.select-all")[0];
+      var total = $("input.select-item").length;
+      var len = $("input.select-item:checked:checked").length;
+      all.checked = len===total;
+  });
+  
+});  
