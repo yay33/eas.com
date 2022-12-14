7c8dd1bc1c5e88e6fe2e3bca3e7ec504bfdaadde
qwerty_xyecoc <max6kotlov@gmail.com>
2022-12-14 18:56:41 +0500

    modals

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
+console.log('123')
+
+//var modal = "#mymodal"
+//modal.on('shown.bs.modal', function () {
+ // $('#myInput').trigger('focus')
+//})
+
+
+var myModal = document.getElementById('modal_cool')
+var myInput = document.getElementById('myBtn')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
 })
+
